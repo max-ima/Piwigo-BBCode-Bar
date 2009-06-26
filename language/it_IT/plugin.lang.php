ac27b19973ffd67608622b2a7a570e16f5504041
riopwg <riopwg@piwigo.org>
2009-06-26 21:31:38 +0000

    Langue IT
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@3469 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+	global $lang;
+	
+	$lang['admin_liste'] 		= "Lista :";	
+	
+	$lang['p_help'] 		= "Paragrafo : [p]paragrafo[/p]";	
+	$lang['b_help'] 		= "Grassetto : [b]grassetto[/b]";
+	$lang['i_help'] 		= "Corsivo : [i]Corsivo[/i]";
+	$lang['u_help'] 		= "Sottolineato : [u]sottolineato[/u]";	
+	$lang['s_help'] 		= "Barrato : [s]Barrato[/s]";
+	
+	$lang['center_help'] 	= "Centrato : [center]centrato[/center]";
+	$lang['right_help'] 	= "Destra : [right]destra[/right]";
+	$lang['ul_help'] 		= "Lista : [ul][li]elemento[/li][/ul]";
+	$lang['ol_help'] 		= "Lista Numerica : [ol][li]elemento[/li][/ol]";
+	
+	$lang['quote_help'] 	= "Citazione : [quote]citazione[/quote]";
+	$lang['img_help'] 		= "Immagine : [img]immagine[/img]";
+	$lang['url_help'] 		= "URL : [url=Page URL]Nome della pagina[/url]";
+	$lang['mail_help'] 		= "E-mail : [email]Email[/email]";	
+	
+	$lang['size_help'] 		= "Dimensione testo : [size=X]testo[/size]";
+	$lang['tiny'] 		= "Piccolissimo";
+	$lang['small'] 		= "Piccolo";
+	$lang['normal'] 	= "Normale";
+	$lang['large'] 		= "Grande";
+	$lang['huge'] 		= "Grandissimo";	
+	
+	$lang['fc_help'] 	= "Colore del testo  : [color=couleur]testo[/color]";
+	$lang['default_help']	= "Default";
+	$lang['maroon_help'] 	= "Rosso scuro";		
+	$lang['red_help'] 		= "Rosso";			
+	$lang['orange_help'] 	= "Arancione";	 	
+	$lang['brown_help'] 	= "Marrone";
+	$lang['yellow_help'] 	= "Giallo";	 	
+	$lang['green_help'] 	= "Verde"; 	
+	$lang['olive_help'] 	= "Oliva";	 	
+	$lang['cyan_help'] 		= "Ciano";
+	$lang['blue_help'] 		= "Blu";	
+	$lang['darkblue_help']	= "Blu Scuro";
+	$lang['indigo_help'] 	= "Indaco";
+	$lang['violet_help'] 	= "Viola";
+	$lang['white_help'] 	= "Bianco";
+	$lang['black_help'] 	= "Nero";
+	
+	$lang['preview'] 		= "Previsualisare";
+	
+	$lang['help']			= "Trucco : Un formattaggio può essere applicato ad un testo selezionato.";
+	
+	$lang['repicon']		= "Directory delle icone";
+  
+  $lang['bbcb_config_saved'] = "Configurazione registrata";
+?>
