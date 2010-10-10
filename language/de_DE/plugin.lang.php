cb499cc9a982f7518ee2b37354c85ad3bc2dd561
duke <duke@piwigo.org>
2010-10-10 22:04:51 +0000

    added de_DE
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@7156 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+	global $lang;
+	
+	$lang['admin_liste'] 		= "Liste :";	
+	
+	$lang['p_help'] 		= "Paragraph : [p]Paragraph[/p]";	
+	$lang['b_help'] 		= "Fett : [b]fett[/b]";
+	$lang['i_help'] 		= "Kursiv : [i]kursiv[/i]";
+	$lang['u_help'] 		= "Unterstrichen : [u]unterstrichen[/u]";	
+	$lang['s_help'] 		= "Durchgestrichen  : [s]durchgestrichen [/s]";
+	
+	$lang['center_help'] 		= "Mittig : [center]mittig[/center]";
+	$lang['right_help'] 		= "Rechts : [right]rechts[/right]";
+	$lang['ul_help'] 		= "Un-nummerierte Liste : [ul][li]Element[/li][/ul]";
+	$lang['ol_help'] 		= "Nummerierte Liste : [ol][li]Element[/li][/ol]";
+	
+	$lang['quote_help'] 		= "Zitat : [quote]Zitat[/quote]";
+	$lang['img_help'] 		= "Bild : [img]Bild[/img]";
+	$lang['url_help'] 		= "URL : [url=URL]Titel[/url]";
+	$lang['mail_help'] 		= "E-mail : [email]E-mail[/email]";	
+	
+	$lang['size_help'] 		= "Textgröße : [size=X]Text[/size]";
+	$lang['tiny'] 			= "Winzig";
+	$lang['small'] 			= "Klein";
+	$lang['normal'] 		= "Normal";
+	$lang['large'] 			= "Groß";
+	$lang['huge'] 			= "Riesig";	
+	
+	$lang['fc_help'] 		= "Textfarbe : [color=farbe]Text[/color]";
+	$lang['default_help']		= "Standard";
+	$lang['maroon_help'] 		= "Dunkelrot";		
+	$lang['red_help'] 		= "Rot";			
+	$lang['orange_help'] 		= "Orange";	 	
+	$lang['brown_help'] 		= "Braun";
+	$lang['yellow_help'] 		= "Gelb";	 	
+	$lang['green_help'] 		= "Grün"; 	
+	$lang['olive_help'] 		= "Olive";	 	
+	$lang['cyan_help'] 		= "Cyan";
+	$lang['blue_help'] 		= "Blau";	
+	$lang['darkblue_help']		= "Dunkelblau";
+	$lang['indigo_help'] 		= "Indigo";
+	$lang['violet_help'] 		= "Violett";
+	$lang['white_help'] 		= "Weiß";
+	$lang['black_help'] 		= "Schwarz";
+	
+	$lang['preview'] 		= "Vorschau";
+	
+	$lang['help']			= "Ein Kniff: Das Format kann dem ausgewählten Text zugewiesen werden.";
+	
+	$lang['repicon']		= "Iconverzeichnis.";
+  
+  $lang['bbcb_config_saved'] = "Konfiguration gespeichert";
+?>
