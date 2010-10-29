53193598399fa21dff15313bd1277b5e3f680092
ddtddt <ddtddt@piwigo.org>
2010-10-29 16:21:41 +0000

    [extensions] - bbcode_bar - Add ru_RU thanks to kiddy-admin
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@7484 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+	global $lang; /*é*/
+	
+	$lang['admin_liste'] 		= "Список:";	
+	
+	$lang['p_help'] 		= "Параграф: [p]параграф[/p]";	
+	$lang['b_help'] 		= "Жирный: [b]жирный[/b]";
+	$lang['i_help'] 		= "Италик: [i]италик[/i]";
+	$lang['u_help'] 		= "Подчернутый: [u]подчеркнутый[/u]";	
+	$lang['s_help'] 		= "Зачеркнутый: [s]зачеркнутый [/s]";
+	
+	$lang['center_help'] 	= "Центр: [center]центр[/center]";
+	$lang['right_help'] 	= "Право: [right]право[/right]";
+	$lang['ul_help'] 		= "Список ненумерованный: [ul][li]элемент[/li][/ul]";
+	$lang['ol_help'] 		= "Список нумерованный: [ol][li]элемент[/li][/ol]";
+	
+	$lang['quote_help'] 	= "Цитата: [quote]цитата[/quote]";
+	$lang['img_help'] 		= "Картинка: [img]картинка[/img]";
+	$lang['url_help'] 		= "URL: [url=URL]Название[/url]";
+	$lang['mail_help'] 		= "E-mail: [email]электронный адрес[/email]";	
+	
+	$lang['size_help'] 		= "Размер шрифта: [size=X]текст[/size]";
+	$lang['tiny'] 		= "Крошечный";
+	$lang['small'] 		= "Маленький";
+	$lang['normal'] 	= "Нормальный";
+	$lang['large'] 		= "Большой";
+	$lang['huge'] 		= "Громадный";	
+	
+	$lang['fc_help'] 	= "Цвет шрифта : [color=color]текст[/color]";
+	$lang['default_help']	= "По умолчанию";
+	$lang['maroon_help'] 	= "Бордовый";		
+	$lang['red_help'] 		= "Красный";			
+	$lang['orange_help'] 	= "Оранжевый";	 	
+	$lang['brown_help'] 	= "Коричневый";
+	$lang['yellow_help'] 	= "Желтый";	 	
+	$lang['green_help'] 	= "Зеленый"; 	
+	$lang['olive_help'] 	= "Оливковый";	 	
+	$lang['cyan_help'] 		= "Небесно голубой";
+	$lang['blue_help'] 		= "Голубой";	
+	$lang['darkblue_help']	= "Синий";
+	$lang['indigo_help'] 	= "Индиго";
+	$lang['violet_help'] 	= "Фиолетовый";
+	$lang['white_help'] 	= "Белый";
+	$lang['black_help'] 	= "Черный";
+	
+	$lang['preview'] 		= "Предпросмотр";
+	
+	$lang['help']			= "Легко: Форматирование может быть применено к выбранному тексту.";
+	
+	$lang['repicon']		= "Директория с иконками";
+  
+  $lang['bbcb_config_saved'] = "Конфигурация сохранена";
+?>
