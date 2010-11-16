d0b22ff7c140b1cbdad3353724d4125a328444ba
ddtddt <ddtddt@piwigo.org>
2010-11-16 07:01:24 +0000

    [extensions] - bbcode_bar -  add cs_CZ thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@7787 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+	global $lang;
+	
+		$lang['admin_liste'] 		= "Seznam :";	
+	
+	$lang['p_help'] 		= "Odstavec : [p]odstavec[/p]";	
+	$lang['b_help'] 		= "Tučné : [b]tučné[/b]";
+	$lang['i_help'] 		= "Kurzíva : [i]kurzíva[/i]";
+	$lang['u_help'] 		= "Podtržené : [u]podtržené[/u]";	
+	$lang['s_help'] 		= "Přeškrtnuté : [s]přeškrtnuto[/s]";
+	
+	$lang['center_help'] 	= "Uprostřed : [center]uprostřed[/center]";
+	$lang['right_help'] 	= "Na pravo : [right]na pravo[/right]";
+	$lang['ul_help'] 		= "Jednoduchý seznam : [ul][li]element[/li][/ul]";
+	$lang['ol_help'] 		= "Číslovaný seznam: [ol][li]element[/li][/ol]";
+	
+	$lang['quote_help'] 	= "Citace : [quote]citát[/quote]";
+	$lang['img_help'] 		= "Obrázek : [img]obrázek[/img]";
+	$lang['url_help'] 		= "URL : [url=URL]Nadpis[/url]";
+	$lang['mail_help'] 		= "E-mail : [email]Email[/email]";	
+	
+	$lang['size_help'] 		= "Velikost fontu : [size=X]text[/size]";
+	$lang['tiny'] 		= "Drobná";
+	$lang['small'] 		= "Malá";
+	$lang['normal'] 	= "Normální";
+	$lang['large'] 		= "Veliký";
+	$lang['huge'] 		= "Obří";	
+	
+	$lang['fc_help'] 	= "Barva písma : [color=color]text[/color]";
+	$lang['default_help']	= "Defaultní";
+	$lang['maroon_help'] 	= "Tmavě červená";		
+	$lang['red_help'] 		= "Červená";			
+	$lang['orange_help'] 	= "Oranžová";	 	
+	$lang['brown_help'] 	= "Hnědá";
+	$lang['yellow_help'] 	= "Žlutá";	 	
+	$lang['green_help'] 	= "Zelená"; 	
+	$lang['olive_help'] 	= "Olivová";	 	
+	$lang['cyan_help'] 		= "Azurová";
+	$lang['blue_help'] 		= "Modrá";	
+	$lang['darkblue_help']	= "Tmavá modrá";
+	$lang['indigo_help'] 	= "Indigo";
+	$lang['violet_help'] 	= "Fialková";
+	$lang['white_help'] 	= "Bílá";
+	$lang['black_help'] 	= "Černá";
+	
+	$lang['preview'] 		= "Náhled";
+	
+	$lang['help']			= "Nejjednodušeji: formátování může být aplikováno jen na označený text.";
+	
+	$lang['repicon']		= "Složka ikon.";
+  
+  $lang['bbcb_config_saved'] = "Konfigurace uložena";
+?>
