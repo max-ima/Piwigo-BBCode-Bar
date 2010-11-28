3961c7ca23a1cfd120a9735f56d4566c168e9ab4
grum <grum@piwigo.org>
2010-11-28 21:14:08 +0000

    fix invalid space character (not defined as 0x20 and cause PHP chrashes)
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@7924 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -1,53 +1,53 @@
 <?php
-	global $lang;
-	
-	$lang['admin_liste'] 		= "Lista :";	
-	
-	$lang['p_help'] 		= "Párrafo : [p]párrafo[/p]";	
-	$lang['b_help'] 		= "Gordo : [b]gordo[/b]";
-	$lang['i_help'] 		= "Itálico : [i]itálico[/i]";
-	$lang['u_help'] 		= "Subrayado : [u]subrayado[/u]";	
-	$lang['s_help'] 		= "Tachado : [s]tachado[/s]";
-	
-	$lang['center_help'] 	= "Centrado : [center]centrado[/center]";
-	$lang['right_help'] 	= "Derecha : [right]derecha[/right]";
-	$lang['ul_help'] 		= "Lista : [ul][li]elemento[/li][/ul]";
-	$lang['ol_help'] 		= "Lista Numerada : [ol][li]elemento[/li][/ol]";
-	
-	$lang['quote_help'] 	= "Citada : [quote]Citación[/quote]";
-	$lang['img_help'] 		= "Imagen : [img]Imagen[/img]";
-	$lang['url_help'] 		= "URL : [url=Page URL]Nombre de la página[/url]";
-	$lang['mail_help'] 		= "Email : [email]Email[/email]";	
-	
-	$lang['size_help'] 		= "Tamaño carácteres : [size=X]texto[/size]";
-	$lang['tiny'] 		= "Todo pequeño";
-	$lang['small'] 		= "Pequeño";
-	$lang['normal'] 	= "Normal";
-	$lang['large'] 		= "Grande";
-	$lang['huge'] 		= "Muy grande";	
-	
-	$lang['fc_help'] 	= "Color de los carácteres  : [color=color]texto[/color]";
-	$lang['default_help']	= "Defecto";
-	$lang['maroon_help'] 	= "Rojo oscuro";		
-	$lang['red_help'] 		= "Rojo";			
-	$lang['orange_help'] 	= "Naranja";	 	
-	$lang['brown_help'] 	= "Castaño";
-	$lang['yellow_help'] 	= "amarillo";	 	
-	$lang['green_help'] 	= "Verde"; 	
-	$lang['olive_help'] 	= "Aceituna";	 	
-	$lang['cyan_help'] 		= "Cian";
-	$lang['blue_help'] 		= "Azul";	
-	$lang['darkblue_help']	= "Azul oscuro";
-	$lang['indigo_help'] 	= "Índigo";
-	$lang['violet_help'] 	= "Morado";
-	$lang['white_help'] 	= "Blanco";
-	$lang['black_help'] 	= "Negro";
-	
-	$lang['preview'] 		= "Visualizar";
-	
-	$lang['help']			= "Astucia: un formato puede ser aplicado sobre el texto seleccionado.";
-	
-	$lang['repicon']		= "Repertorio de los iconos."
-	
-	$lang['bbcb_config_saved'] = "Configuración registrada";
+  global $lang;
+
+  $lang['admin_liste']    = "Lista :";
+
+  $lang['p_help']     = "Párrafo : [p]párrafo[/p]";
+  $lang['b_help']     = "Gordo : [b]gordo[/b]";
+  $lang['i_help']     = "Itálico : [i]itálico[/i]";
+  $lang['u_help']     = "Subrayado : [u]subrayado[/u]";
+  $lang['s_help']     = "Tachado : [s]tachado[/s]";
+
+  $lang['center_help']  = "Centrado : [center]centrado[/center]";
+  $lang['right_help']   = "Derecha : [right]derecha[/right]";
+  $lang['ul_help']    = "Lista : [ul][li]elemento[/li][/ul]";
+  $lang['ol_help']    = "Lista Numerada : [ol][li]elemento[/li][/ol]";
+
+  $lang['quote_help']   = "Citada : [quote]Citación[/quote]";
+  $lang['img_help']     = "Imagen : [img]Imagen[/img]";
+  $lang['url_help']     = "URL : [url=Page URL]Nombre de la página[/url]";
+  $lang['mail_help']    = "Email : [email]Email[/email]";
+
+  $lang['size_help']    = "Tamaño carácteres : [size=X]texto[/size]";
+  $lang['tiny']     = "Todo pequeño";
+  $lang['small']    = "Pequeño";
+  $lang['normal']   = "Normal";
+  $lang['large']    = "Grande";
+  $lang['huge']     = "Muy grande";
+
+  $lang['fc_help']  = "Color de los carácteres  : [color=color]texto[/color]";
+  $lang['default_help'] = "Defecto";
+  $lang['maroon_help']  = "Rojo oscuro";
+  $lang['red_help']     = "Rojo";
+  $lang['orange_help']  = "Naranja";
+  $lang['brown_help']   = "Castaño";
+  $lang['yellow_help']  = "amarillo";
+  $lang['green_help']   = "Verde";
+  $lang['olive_help']   = "Aceituna";
+  $lang['cyan_help']    = "Cian";
+  $lang['blue_help']    = "Azul";
+  $lang['darkblue_help']  = "Azul oscuro";
+  $lang['indigo_help']  = "Índigo";
+  $lang['violet_help']  = "Morado";
+  $lang['white_help']   = "Blanco";
+  $lang['black_help']   = "Negro";
+
+  $lang['preview']    = "Visualizar";
+
+  $lang['help']     = "Astucia: un formato puede ser aplicado sobre el texto seleccionado.";
+
+  $lang['repicon']    = "Repertorio de los iconos.";
+
+  $lang['bbcb_config_saved'] = "Configuración registrada";
 ?>
