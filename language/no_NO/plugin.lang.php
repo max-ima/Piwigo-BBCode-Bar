3b22270e8919da98c0cbe8d0e4d200d01b06143e
ddtddt <ddtddt@piwigo.org>
2011-02-03 16:10:10 +0000

    [extensions] - bbcode_bar - add no_NO (Norwegian) thanks to atlew
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@9082 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/no_NO/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+	global $lang;
+	
+$lang['admin_liste']         = "Liste :";
+
+$lang['p_help']         = "Paragraf : [p]Paragraf[/p]";
+$lang['b_help']         = "Fet : [b]Fet[/b]";
+$lang['i_help']         = "Italic : [i]italic[/i]";
+$lang['u_help']         = "Understreket : [u]understreket[/u]";
+$lang['s_help']         = "Stripet  : [s]stripet [/s]";
+
+$lang['center_help']     = "Senter : [center]senter[/center]";
+$lang['right_help']     = "Høyre : [right]høyre[/right]";
+$lang['ul_help']         = "Uordnet Liste : [ul][li]element[/li][/ul]";
+$lang['ol_help']         = "Ordnet Liste : [ol][li]element[/li][/ol]";
+$lang['quote_help']     = "Spørring : [quote]spørring[/quote]";
+$lang['img_help']         = "Img : [img]img[/img]";
+$lang['url_help']         = "URL : [url=URL]Tittel[/url]";
+$lang['mail_help']         = "E-mail : [email]Email[/email]";
+
+$lang['size_help']         = "Skrift størrelse : [size=X]tekst[/size]";
+$lang['tiny']         = "Mini";
+$lang['small']         = "Liten";
+$lang['normal']     = "Normal";
+$lang['large']         = "Stor";
+$lang['huge']         = "Mega";
+
+$lang['fc_help']     = "Font farge : [color=color]tekst[/color]";
+$lang['default_help']    = "Standard";
+$lang['maroon_help']     = "Maroon";
+$lang['red_help']         = "Rød";
+$lang['orange_help']     = "Orange";
+$lang['brown_help']     = "Brun";
+$lang['yellow_help']     = "Gul";
+$lang['green_help']     = "Grønn";
+$lang['olive_help']     = "Oliven";
+$lang['cyan_help']         = "Cyan";
+$lang['blue_help']         = "Blå";
+$lang['darkblue_help']    = "Mørk Blå";
+$lang['indigo_help']     = "Indigo";
+$lang['violet_help']     = "Fiolett";
+$lang['white_help']     = "Vit";
+$lang['black_help']     = "Svart";
+
+$lang['preview']         = "Forhåndsvis";
+
+$lang['help']            = "Enkel måte: En formatering kan gjøres på den valgte teksten.";
+
+$lang['repicon']        = "Ikon mappe.";
+
+$lang['bbcb_config_saved'] = "Konfigurasjon lagret";
+?>
