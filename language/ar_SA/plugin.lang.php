ef99eb693b4e388e26fe6307c1151e0b5543063c
ddtddt <ddtddt@piwigo.org>
2011-04-10 19:41:37 +0000

    [extensions] - bbcode_bar - add help ar_SA thanks to bakir
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@10260 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+	
+$lang['center_help']     = "منتصف: [center]منتصف[/center]";
+$lang['right_help']     = "يمين : [right]يمين[/right]";
+$lang['ul_help']         = "قائمة غير مرتبة : [ul][li]العنصؤ[/li][/ul]";
+$lang['ol_help']         = "قائمة مرتبة : [ol][li]العنصر[/li][/ol]";
+$lang['li_help']         = "عنصر القائمة : [li]العنصر[/li]";
+ 
+$lang['quote_help']     = "الاقتباس : [quote]الاقتباس[/quote]";
+$lang['img_help']         = "الصورة : [img]الصورة[/img]";
+$lang['url_help']         = "الرابط : [url=URL]العنوان[/url]";
+$lang['mail_help']         = "البريد : [email]البريد[/email]";
+ 
+$lang['size_help']         = "حجم الخط : [size=X]النص[/size]";
+$lang['tiny']             = "صغير جدا";
+$lang['small']             = "صغير";
+$lang['normal']         = "عادي";
+$lang['large']             = "كبير";
+$lang['huge']             = "كبير جدا";
+ 
+$lang['color_help']     = "لون الخط : [color=color]النص[/color]";
+$lang['red_help']         = "احمر";
+$lang['orange_help']     = "برتقالي";
+$lang['yellow_help']     = "اصفر";
+$lang['green_help']     = "اخضر";
+$lang['blue_help']         = "ازرق";
+$lang['purple_help']     = "زهري";
+$lang['white_help']     = "ابيض";
+$lang['grey_help']         = "رمادي";
+$lang['black_help']     = "اسود";
+  
+?>
