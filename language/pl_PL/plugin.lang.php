0daf7b2b04ccb0cfeabd6b4068b6135f28566786
ddtddt <ddtddt@piwigo.org>
2011-06-14 06:35:39 +0000

    [extensions] - bbcode_bar - add PL thanks to T00kie
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@11351 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+
+$lang['p_help'] = "Paragraf : [p]Paragraf[/p]";
+$lang['b_help'] = "Pogrubione : [b]pogrubione[/b]";
+$lang['i_help'] = "Pochylone : [i]pochylone[/i]";
+$lang['u_help'] = "Podkreślone : [u]podkreślone[/u]";
+$lang['s_help'] = "Przekreślone  : [s]przekreślone[/s]";
+
+$lang['center_help'] = "Wyśrodkowanie : [center]wyśrodkowanie[/center]";
+$lang['right_help'] = "Wyrównanie do prawej : [right]wyrównanie do prawej[/right]";
+$lang['ul_help'] = "Lista nieuporządkowana : [ul][li]element[/li][/ul]";
+$lang['ol_help'] = "Lista uporządkowana : [ol][li]element[/li][/ol]";
+$lang['li_help'] = "Element listy : [li]element[/li]";
+
+$lang['quote_help'] = "Cytat : [quote]cytat[/quote]";
+$lang['img_help'] = "Obrazek : [img]obrazek[/img]";
+$lang['url_help'] = "URL : [url=URL]Title[/url]";
+$lang['mail_help'] = "E-mail : [email]Email[/email]";
+
+$lang['size_help'] = "Rozmiar czcionki : [size=X]tekst[/size]";
+$lang['tiny'] = "Malutka";
+$lang['small'] = "Mała";
+$lang['normal'] = "Normalna";
+$lang['large'] = "Duża";
+$lang['huge'] = "Ogromna";
+
+$lang['color_help'] = "Kolor czcionki : [color=color]tekst[/color]";
+$lang['red_help'] = "Czerwony";
+$lang['orange_help'] = "Pomarańczowy";
+$lang['yellow_help'] = "Żółty";
+$lang['green_help'] = "Zielony";
+$lang['blue_help'] = "Niebieski";
+$lang['purple_help'] = "Fioletowy";
+$lang['white_help'] = "Biały";
+$lang['grey_help'] = "Szary";
+$lang['black_help'] = "Czarny";
+  
+?>
