4b7fec394a403591707d9e3debb0fe7070f125a3
ddtddt <ddtddt@piwigo.org>
2011-06-22 21:11:21 +0000

    [extensions] - bbcode_bar - add NL (Dutch - Nederlands) thanks to sleepless
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@11493 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+
+$lang['p_help'] = "Alinea : [p]Paragraph[/p]";
+$lang['b_help'] = "Vet : [b]bold[/b]";
+$lang['i_help'] = "Cursief : [i]italic[/i]";
+$lang['u_help'] = "Onderstreept : [u]underline[/u]";
+$lang['s_help'] = "Doorgestreept  : [s]striped[/s]";
+
+$lang['center_help'] = "Gecentreerd : [center]center[/center]";
+$lang['right_help'] = "Rechts : [right]right[/right]";
+$lang['ul_help'] = "Ongesorteerde lijst : [ul][li]element[/li][/ul]";
+$lang['ol_help'] = "Gesorteerde lijst : [ol][li]element[/li][/ol]";
+$lang['li_help'] = "Lijst element : [li]element[/li]";
+
+$lang['quote_help'] = "Quote : [quote]quote[/quote]";
+$lang['img_help'] = "Afbeelding : [img]picture[/img]";
+$lang['url_help'] = "URL : [url=URL]Title[/url]";
+$lang['mail_help'] = "E-mail : [email]Email[/email]";
+
+$lang['size_help'] = "Font grootte : [size=X]text[/size]";
+$lang['tiny'] = "Zeer klein";
+$lang['small'] = "Klein";
+$lang['normal'] = "Normaal";
+$lang['large'] = "Groot";
+$lang['huge'] = "Zeer groot";
+
+$lang['color_help'] = "Font kleur : [color=color]text[/color]";
+$lang['red_help'] = "Rood";
+$lang['orange_help'] = "Orange";
+$lang['yellow_help'] = "Geel";
+$lang['green_help'] = "Groen";
+$lang['blue_help'] = "Blauw";
+$lang['purple_help'] = "Purper";
+$lang['white_help'] = "Wit";
+$lang['grey_help'] = "Grijs";
+$lang['black_help'] = "Zwart";
+  
+?>
