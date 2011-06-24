ee029a2e55c86293dde361f2e44b2ae94392a280
ddtddt <ddtddt@piwigo.org>
2011-06-24 07:44:19 +0000

    [extensions] - bbcode_bar - update NL (Dutch - Nederlands) thanks to sleepless
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@11503 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -1,30 +1,29 @@
 <?php
 
-$lang['p_help'] = "Alinea : [p]Paragraph[/p]";
-$lang['b_help'] = "Vet : [b]bold[/b]";
-$lang['i_help'] = "Cursief : [i]italic[/i]";
-$lang['u_help'] = "Onderstreept : [u]underline[/u]";
-$lang['s_help'] = "Doorgestreept  : [s]striped[/s]";
+$lang['p_help'] = "Alinea : [p]alinea[/p]";
+$lang['b_help'] = "Vet : [b]hier komt  vette tekst[/b]";
+$lang['i_help'] = "Cursief : [i]hier komt cursieve tekst[/i]";
+$lang['u_help'] = "Onderstreept : [u]hier komt onderstreepte tekst[/u]";
+$lang['s_help'] = "Doorgestreept  : [s]hier komt doorgestreepte tekst[/s]";
 
-$lang['center_help'] = "Gecentreerd : [center]center[/center]";
-$lang['right_help'] = "Rechts : [right]right[/right]";
-$lang['ul_help'] = "Ongesorteerde lijst : [ul][li]element[/li][/ul]";
-$lang['ol_help'] = "Gesorteerde lijst : [ol][li]element[/li][/ol]";
-$lang['li_help'] = "Lijst element : [li]element[/li]";
+$lang['center_help'] = "Gecentreerd : [center]deze tekst wordt gecentreerd[/center]";
+$lang['right_help'] = "Rechts : [right]deze tekst wordt rechts uitgelijnd[/right]";
+$lang['ul_help'] = "Ongesorteerde lijst : [ul][li]hier komt een lijst element[/li][/ul]";
+$lang['ol_help'] = "Gesorteerde lijst : [ol][li]hier komt een lijst element[/li][/ol]";
+$lang['li_help'] = "Lijst element : [li]hier komt een lijst element[/li]";
 
-$lang['quote_help'] = "Quote : [quote]quote[/quote]";
-$lang['img_help'] = "Afbeelding : [img]picture[/img]";
-$lang['url_help'] = "URL : [url=URL]Title[/url]";
-$lang['mail_help'] = "E-mail : [email]Email[/email]";
-
-$lang['size_help'] = "Font grootte : [size=X]text[/size]";
+$lang['quote_help'] = "Quote: [quote]hier komt de aangehaalde tekst[/quote]";
+$lang['img_help'] = "Afbeelding : [img]hier komt de link naar de afbeelding, bv: http://www.site/afbeelding.jpg[/img]";
+$lang['url_help'] = "URL : [hier komt bv: http://www.hetwebdadres]Na het haakje komt: de Titel voor de link[/url]";
+$lang['mail_help'] =   "E-mail : [email]hier komt het e-mail adres[/email]";
+$lang['size_help'] = "Letter grootte : [size=X]tekst[/size]";
 $lang['tiny'] = "Zeer klein";
 $lang['small'] = "Klein";
 $lang['normal'] = "Normaal";
 $lang['large'] = "Groot";
 $lang['huge'] = "Zeer groot";
 
-$lang['color_help'] = "Font kleur : [color=color]text[/color]";
+$lang['color_help'] = "Tekst kleur : [color=color]text[/color]";
 $lang['red_help'] = "Rood";
 $lang['orange_help'] = "Orange";
 $lang['yellow_help'] = "Geel";
