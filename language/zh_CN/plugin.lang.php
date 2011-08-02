6910cd24e66deb0d52c59292ea30c0d4febb87f7
ddtddt <ddtddt@piwigo.org>
2011-08-02 19:45:48 +0000

    [extensions] - bbcode_bar - update zh_CN - 简体中文 - Chinese thanks to Ralph.Bai
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@11897 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+
+$lang['p_help'] = "段落: [p]段落[/p]";
+$lang['b_help'] = "加粗 : [b]加粗[/b]";
+$lang['i_help'] = "斜体 : [i]斜体[/i]";
+$lang['u_help'] = "下划线 : [u]下划线[/u]";
+$lang['s_help'] = "横线 : [s]横线[/s]";
+
+$lang['center_help'] = "居中 : [center]居中[/center]";
+$lang['right_help'] = "居右 : [right]居右[/right]";
+$lang['ul_help'] = "无序表列 : [ul][li]元素[/li][/ul]";
+$lang['ol_help'] = "有序列表 : [ol][li]元素[/li][/ol]";
+$lang['li_help'] = "列表元素 : [li]元素[/li]";
+
+$lang['quote_help'] = "引言 : [quote]引言[/quote]";
+$lang['img_help'] = "图片 : [img]图片[/img]";
+$lang['url_help'] = "URL : [url=URL]Title[/url]";
+$lang['mail_help'] = "E-mail : [email]Email[/email]";
+
+$lang['size_help'] = "字体大小 : [size=X]文本[/size]";
+$lang['tiny'] = "极细";
+$lang['small'] = "小号";
+$lang['normal'] = "标准";
+$lang['large'] = "大号";
+$lang['huge'] = "超大";
+
+$lang['color_help'] = "字体颜色 : [color=color]文本[/color]";
+$lang['red_help'] = "红色";
+$lang['orange_help'] = "橙色";
+$lang['yellow_help'] = "黄色";
+$lang['green_help'] = "绿色";
+$lang['blue_help'] = "蓝色";
+$lang['purple_help'] = "紫色";
+$lang['white_help'] = "白色";
+$lang['grey_help'] = "灰色";
+$lang['black_help'] = "黑色";
+  
+?>
