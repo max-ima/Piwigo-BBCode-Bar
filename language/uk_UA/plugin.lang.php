5f6c418bb82ea9cdbfce3d990dbcf5d433b2bf30
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-26 07:42:37 +0000

    [BBCode Bar] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@13362 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Black'] = 'Чорний';
+$lang['Blue'] = 'Блакитний';
+$lang['Bold : [b]bold[/b]'] = 'Напівжирний шрифт: [b]Напівжирний шрифт[/b]';
+$lang['Center : [center]center[/center]'] = 'Центр: [center]текст[/center]';
+$lang['E-mail : [email]Email[/email]'] = 'Емейл: [email]Емейл[/email]';
+$lang['Font color : [color=color]text[/color]'] = 'Колір шрифту: [color=color]текст[/color]';
+$lang['Font size : [size=X]text[/size]'] = 'Розмір шрифту: [size=X]текст[/size]';
+$lang['Green'] = 'Зелений';
+$lang['Grey'] = 'Сірий';
+$lang['huge'] = 'Величезний';
+$lang['Italic : [i]italic[/i]'] = 'Курсив: [i]Курсив[/i]';
+$lang['large'] = 'Великий';
+$lang['List element : [li]element[/li]'] = 'Список елементів: [li]елемент[/li]';
+$lang['normal'] = 'Нормальний';
+$lang['Orange'] = 'Помаранчевий';
+$lang['Ordered list : [ol][li]element[/li][/ol]'] = 'Нумерований список: [ol][li]елемент[/li][/ol]';
+$lang['Paragraph : [p]Paragraph[/p]'] = 'Абзац: [p]Абзац[/p]';
+$lang['Picture : [img]picture[/img]'] = 'Зображення: [img]зображення[/img]';
+$lang['Purple'] = 'Пурпурний';
+$lang['Quote : [quote]quote[/quote]'] = 'Цитата: [quote]цитата[/quote]';
+$lang['Red'] = 'Червоний';
+$lang['Right : [right]right[/right]'] = 'Праворуч: [right]текст[/right]';
+$lang['small'] = 'Маленький';
+$lang['Striped  : [s]striped[/s]'] = 'Смугастий: [s]текст[/s]';
+$lang['tiny'] = 'Крихітний';
+$lang['Underline : [u]underline[/u]'] = 'Підкреслений: [u]текст[/u]';
+$lang['Unordered list : [ul][li]element[/li][/ul]'] = 'Неупорядкований список: [ul][li]елемент[/li][/ul]';
+$lang['URL : [url=URL]Title[/url]'] = 'Посилання: [url=URL]Назва[/url]';
+$lang['White'] = 'Білий';
+$lang['Yellow'] = 'Жовтий';
+?>
