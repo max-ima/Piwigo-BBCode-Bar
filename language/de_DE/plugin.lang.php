ce97d44ff3c96c61b58f6d5b3ce0c15376bee8f1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-23 12:37:15 +0000

    [BBCode Bar] Update de_DE, thanks to : Sugar888
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@13670 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -3,27 +3,27 @@
 $lang['Paragraph : [p]Paragraph[/p]'] = 'Paragraph : [p]Paragraph[/p]';
 $lang['Bold : [b]bold[/b]'] = 'Fett : [b]fett[/b]';
 $lang['Italic : [i]italic[/i]'] = 'Kursiv : [i]kursiv[/i]';
-$lang['Underline : [u]underline[/u]'] = 'Unterstrichen : [u]unterstrichen[/u]';
+$lang['Underline : [u]underline[/u]'] = 'Unterstreichen : [u]unterstreichen[/u]';
 $lang['Striped  : [s]striped[/s]'] = 'Durchgestrichen  : [s]durchgestrichen [/s]';
 
-$lang['Center : [center]center[/center]'] = 'Mittig : [center]mittig[/center]';
+$lang['Center : [center]center[/center]'] = 'Zentriert: [center]zentriert[/center]';
 $lang['Right : [right]right[/right]'] = 'Rechts : [right]rechts[/right]';
-$lang['Unordered list : [ul][li]element[/li][/ul]'] = 'Un-nummerierte Liste : [ul][li]Element[/li][/ul]';
-$lang['Ordered list : [ol][li]element[/li][/ol]'] = 'Nummerierte Liste : [ol][li]Element[/li][/ol]';
+$lang['Unordered list : [ul][li]element[/li][/ul]'] = 'Unsortierte Liste : [ul][li]Element[/li][/ul]';
+$lang['Ordered list : [ol][li]element[/li][/ol]'] = 'Sortierte Liste : [ol][li]Element[/li][/ol]';
 
 $lang['Quote : [quote]quote[/quote]'] = 'Zitat : [quote]Zitat[/quote]';
 $lang['Picture : [img]picture[/img]'] = 'Bild : [img]Bild[/img]';
 $lang['URL : [url=URL]Title[/url]'] = 'URL : [url=URL]Titel[/url]';
 $lang['E-mail : [email]Email[/email]'] = 'E-mail : [email]E-mail[/email]';
 
-$lang['Font size : [size=X]text[/size]'] = 'Textgröße : [size=X]Text[/size]';
+$lang['Font size : [size=X]text[/size]'] = 'Schriftgröße : [size=X]Text[/size]';
 $lang['tiny'] = 'Winzig';
 $lang['small'] = 'Klein';
 $lang['normal'] = 'Normal';
 $lang['large'] = 'Groß';
 $lang['huge'] = 'Riesig';
 
-$lang['Font color : [color=color]text[/color]'] = 'Textfarbe : [color=farbe]Text[/color]';
+$lang['Font color : [color=color]text[/color]'] = 'Schriftfarbe : [color=farbe]Text[/color]';
 $lang['Red'] = 'Rot';
 $lang['Orange'] = 'Orange'; 
 $lang['Yellow'] = 'Gelb'; 
@@ -31,7 +31,8 @@ $lang['Green'] = 'Grün';
 $lang['Blue'] = 'Blau';
 $lang['Purple'] = 'Violett';
 $lang['White'] = 'Weiß';
-$lang['Grey'] = 'Grey';
+$lang['Grey'] = 'Grau';
 $lang['Black'] = 'Schwarz';
 
+$lang['List element : [li]element[/li]'] = 'Listeneintrag : [li]Element[/li]';
 ?>
