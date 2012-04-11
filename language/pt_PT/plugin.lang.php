9ad930c51a49b84746451b04b896676bf2835413
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-11 20:18:34 +0000

    [BBCode Bar] Add pt_PT, thanks to : klito
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@14100 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Black'] = 'Preto';
+$lang['Blue'] = 'Azul';
+$lang['Bold : [b]bold[/b]'] = 'Negrito:  [b]bold[/b]';
+$lang['Center : [center]center[/center]'] = 'Centro: [center]center[/center]';
+$lang['E-mail : [email]Email[/email]'] = 'E-mail : [email]Email[/email]';
+$lang['Font color : [color=color]text[/color]'] = 'Cor da letra:[color=color]text[/color]';
+$lang['Font size : [size=X]text[/size]'] = 'Tamanho da letra : [size=X]text[/size]';
+$lang['Green'] = 'Verde';
+$lang['Grey'] = 'Cinza';
+$lang['Italic : [i]italic[/i]'] = 'Itálico: [i]italic[/i]';
+$lang['List element : [li]element[/li]'] = 'Listar elementos: [li]element[/li]';
+$lang['Orange'] = 'Laranja';
+$lang['Ordered list : [ol][li]element[/li][/ol]'] = 'Lista de pedidos: [ol][li]element[/li][/ol]';
+$lang['Paragraph : [p]Paragraph[/p]'] = 'Paragrafo: [p]Paragraph[/p]';
+$lang['Picture : [img]picture[/img]'] = 'Foto: [img]picture[/img]';
+$lang['Purple'] = 'Purpura';
+$lang['Quote : [quote]quote[/quote]'] = 'Valor: [quote]quote[/quote]';
+$lang['Red'] = 'Vermelho';
+$lang['Right : [right]right[/right]'] = 'Direita: [right]right[/right]';
+$lang['Striped  : [s]striped[/s]'] = 'Listado  : [s]striped[/s]';
+$lang['URL : [url=URL]Title[/url]'] = 'URL : [url=URL]Title[/url]';
+$lang['Underline : [u]underline[/u]'] = 'Sublinhado:  [u]underline[/u]';
+$lang['Unordered list : [ul][li]element[/li][/ul]'] = 'Lista de não pedidos: [ul][li]element[/li][/ul]';
+$lang['White'] = 'Branco';
+$lang['Yellow'] = 'Amarelo';
+$lang['huge'] = 'Enorme';
+$lang['large'] = 'Grande';
+$lang['normal'] = 'Normal';
+$lang['small'] = 'Pequeno';
+$lang['tiny'] = 'Fino';
+?>
