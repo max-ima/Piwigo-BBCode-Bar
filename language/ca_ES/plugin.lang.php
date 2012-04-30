fddac3eb4b156c8dedb39ac296aa67056efd8cad
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-30 17:38:35 +0000

    [bbcode_bar] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@14594 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Available options'] = 'Opcions disponibles';
+$lang['Black'] = 'Negre';
+$lang['Blue'] = 'Blau';
+$lang['Bold : [b]bold']['/b'] = 'Negreta: [b]bold[/b]';
+$lang['Center : [center]center']['/center'] = 'Centrat: [center]center[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'Correu electrónic: [email]Email[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Color de la font: [color=color]text[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Mida de font: [size=X]text[/size]';
+$lang['Green'] = 'Verd';
+$lang['Grey'] = 'Gris';
+$lang['Italic : [i]italic']['/i'] = 'Cursiva: [i]italic[/i]';
+$lang['List element : [li]element']['/li'] = 'Elements de la llista: [li]element[/li]';
+$lang['Orange'] = 'Taronja';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Llista ordenada: [ol][li]element[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Paràgraf: [p]Paragraph[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Imatge: [img]picture[/img]';
+$lang['Purple'] = 'Porpra';
+$lang['Quote : [quote]quote']['/quote'] = 'Citar: [quote]quote[/quote]';
+$lang['Red'] = 'Vermell';
+$lang['Right : [right]right']['/right'] = 'Dreta: [right]right[/right]';
+$lang['Striped  : [s]striped']['/s'] = 'Tatxat: [s]striped[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL: [url=URL]Title[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Subratllat: [u]underline[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Llista desordenada: [ul][li]element[/li][/ul]';
+$lang['White'] = 'Blanc';
+$lang['Yellow'] = 'Groc';
+$lang['huge'] = 'Enorme';
+$lang['large'] = 'Gran';
+$lang['normal'] = 'Normal';
+$lang['small'] = 'Petit';
+$lang['tiny'] = 'Diminut';
+?>
