972ec274f4894f91f29e14609ddae319eed4e8e8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-19 18:45:47 +0000

    [bbcode_bar] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@16851 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Available options'] = 'Mevcut seçenekler';
+$lang['Black'] = 'Siyah';
+$lang['Blue'] = 'Mavi';
+$lang['Bold : [b]bold']['/b'] = 'Kalın:[b]kalın[/b]';
+$lang['Center : [center]center']['/center'] = 'Ortalı: [center]ortalı[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'e-Posta: [email]e-Posta[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Yazıtipi rengi: [color=color]yazı[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Yazıtipi büyüklüğü: [size=X]yazı[/size]';
+$lang['Green'] = 'Yeşil';
+$lang['Grey'] = 'Gri';
+$lang['Italic : [i]italic']['/i'] = 'İtalik: [i]italik[/i]';
+$lang['List element : [li]element']['/li'] = 'Liste öğesi: [li]öğe[/li]';
+$lang['Orange'] = 'Turuncu';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Sıralı liste: [ol][li]öğe[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Paragraf: [p]Paragraf[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Resim: [img]resim[/img]';
+$lang['Purple'] = 'Mor';
+$lang['Quote : [quote]quote']['/quote'] = 'Alıntı: [quote]alıntı[/quote]';
+$lang['Red'] = 'Kırmızı';
+$lang['Right : [right]right']['/right'] = 'Sağ: [right]sağ[/right]';
+$lang['Striped  : [s]striped']['/s'] = 'Üzeri çizili: [s]üzeri çizili[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL: [url=URL]İsim[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Altı çizili: [u]altı çizili[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Sıralanmamış liste: [ul][li]öğe[/li][/ul]';
+$lang['White'] = 'Beyaz';
+$lang['Yellow'] = 'Sarı';
+$lang['huge'] = 'Kocaman';
+$lang['large'] = 'Büyük';
+$lang['normal'] = 'Normal';
+$lang['small'] = 'Küçük';
+$lang['tiny'] = 'Minicik';
+?>
