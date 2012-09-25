d21ede7317b1b88a93d8862ed9de61bfcbdf8d1b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-25 05:30:33 +0000

    [bbcode_bar] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@18211 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Striped  : [s]striped']['/s'] = 'Stribet: [s]stribet[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL: [url=URL]titel[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Understregning: [u]understregning[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Usorteret liste: [ul][li]element[/li][/ul]';
+$lang['White'] = 'Hvid';
+$lang['Yellow'] = 'Gul';
+$lang['huge'] = 'Enorm';
+$lang['large'] = 'Stor';
+$lang['normal'] = 'Normal';
+$lang['small'] = 'Lille';
+$lang['tiny'] = 'Mikroskopisk';
+$lang['Available options'] = 'Tilgængelige valgmuligheder';
+$lang['Black'] = 'Sort';
+$lang['Blue'] = 'Blå';
+$lang['Bold : [b]bold']['/b'] = 'Fed: [b]fed[/b]';
+$lang['Center : [center]center']['/center'] = 'Centreret: [center]centreret[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'E-mail : [email]e-mail[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Fontfarve: [color=color]tekst[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Fontstørrelse: [size=X]tekst[/size]';
+$lang['Green'] = 'Grøn';
+$lang['Grey'] = 'Grå';
+$lang['Italic : [i]italic']['/i'] = 'Kursiv: [i]kursiv[/i]';
+$lang['List element : [li]element']['/li'] = 'Listeelement : [li]element[/li]';
+$lang['Orange'] = 'Orange';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Sorteret liste: [ol][li]element[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Afsnit: [p]afsnit[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Billede: [img]billede[/img]';
+$lang['Purple'] = 'Lilla';
+$lang['Quote : [quote]quote']['/quote'] = 'Citat: [quote]citat[/quote]';
+$lang['Red'] = 'Rød';
+$lang['Right : [right]right']['/right'] = 'Højre: [right]højre[/right]';
+?>
