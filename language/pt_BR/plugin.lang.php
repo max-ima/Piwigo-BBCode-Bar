8864ba00fecd05f18e3683fa073a3e88a31f33d8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-30 12:47:30 +0000

    [bbcode_bar] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@20466 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Striped  : [s]striped']['/s'] = 'Listrado: [s]listrado[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL : [url=URL]Título[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Sublinhado: [u]sublinhado[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Lista não ordenada: [ul][li]elemento[/li][/ul]';
+$lang['White'] = 'Branco';
+$lang['Yellow'] = 'Amarelo';
+$lang['huge'] = 'Enorme';
+$lang['large'] = 'Grande';
+$lang['normal'] = 'Normal';
+$lang['small'] = 'Pequeno';
+$lang['tiny'] = 'Minúsculo';
+$lang['Available options'] = 'Opções disponíveis';
+$lang['Black'] = 'Preto';
+$lang['Blue'] = 'Azul';
+$lang['Bold : [b]bold']['/b'] = 'Negrito:[b]bold[/b]';
+$lang['Center : [center]center']['/center'] = 'Centro:[center]centro[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'E-mail : [email]Email[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Cor da Fonte: [color=color]texto[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Tamanho da fonte: [size=X]texto[/size]';
+$lang['Green'] = 'Verde';
+$lang['Grey'] = 'Cinza';
+$lang['Italic : [i]italic']['/i'] = 'Itálico: [i]itálico[/i]';
+$lang['List element : [li]element']['/li'] = 'Listar elemento: [li]elemento[/li]';
+$lang['Orange'] = 'Laranja';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Lista ordenada:  [ol][li]elemento[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Parágrafo: [p]Parágrafo[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Imagem: [img]imagem[/img]';
+$lang['Purple'] = 'Roxo';
+$lang['Quote : [quote]quote']['/quote'] = 'Citação: [quote]citação[/quote]';
+$lang['Red'] = 'Vermelho';
+$lang['Right : [right]right']['/right'] = 'Direita: [right]direita[/right]';
+?>
