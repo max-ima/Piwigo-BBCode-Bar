a23a2e7f714694059fa0ed03ab8e9d304151eeb1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-23 12:11:52 +0000

    [bbcode_bar] Add sv_SE, thanks to : kabo
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@24023 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['normal font'] = 'Normal';
+$lang['small font'] = 'Liten';
+$lang['tiny font'] = 'Pytteliten';
+$lang['White'] = 'Vit';
+$lang['Yellow'] = 'Gul';
+$lang['huge font'] = 'Enorm';
+$lang['large font'] = 'Stor';
+$lang['Black'] = 'Svart';
+$lang['Blue'] = 'Blå';
+$lang['Bold : [b]bold']['/b'] = 'Fetstil: [b]fetstil[/b]';
+$lang['Green'] = 'Grön';
+$lang['Grey'] = 'Grå';
+$lang['Orange'] = 'Orange';
+$lang['Purple'] = 'Lila';
+$lang['Red'] = 'Rod';
+?>
