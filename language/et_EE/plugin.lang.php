412ec7e90d5eb5e00ef284e42f74b5018c42cc25
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-15 06:20:36 +0000

    [bbcode_bar] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@24913 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Nimekirja järjekord : [ol][li]element[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Lõik : [p]Lõik[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Pilt : [img]pilt[/img]';
+$lang['Purple'] = 'Lilla';
+$lang['Quote : [quote]quote']['/quote'] = 'Tsitaat : [quote]tsitaat[/quote]';
+$lang['Red'] = 'Punane';
+$lang['Right : [right]right']['/right'] = 'Paremjoondus:[right]parem[/right]';
+$lang['Striped  : [s]striped']['/s'] = 'Joonetatud  : [s]joonetatud[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL : [url=URL]Pealkiri[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Allajoonitud : [u]Allajoonitud[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Segamini nimekiri : [ul][li]element[/li][/ul]';
+$lang['White'] = 'Valge';
+$lang['Yellow'] = 'kollane';
+$lang['huge font'] = 'Ülisuur';
+$lang['large font'] = 'Suur';
+$lang['normal font'] = 'Normaalne';
+$lang['small font'] = 'Väike';
+$lang['tiny font'] = 'Pisike';
+$lang['Available options'] = 'Võimalikud valikud';
+$lang['Black'] = 'Must';
+$lang['Blue'] = 'Sinine';
+$lang['Bold : [b]bold']['/b'] = 'Paks:[b]paks[/b]';
+$lang['Center : [center]center']['/center'] = 'Keskel:[center]keskel[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'E-mail:[email]Email[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Fondi värv: [color=color]tekst[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Fondi suurus : [size=X]tekst[/size]';
+$lang['Green'] = 'Roheline';
+$lang['Grey'] = 'Hall';
+$lang['Italic : [i]italic']['/i'] = 'Kursiiv:[i]kursiiv[/i]';
+$lang['List element : [li]element']['/li'] = 'Nimekirja element : [li]element[/li]';
+$lang['Orange'] = 'Oranž';
+?>
