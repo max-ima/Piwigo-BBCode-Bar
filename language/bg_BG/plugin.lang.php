b5dd519c6530241eeabce3f30eec88e120413a2f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-25 18:58:31 +0000

    [bbcode_bar] Add bg_BG, thanks to : neddrag
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@25697 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Striped  : [s]striped']['/s'] = 'Раиран : [s]striped[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL : [url=URL]Title[/url]';
+$lang['tiny font'] = 'Миниатюрен';
+$lang['huge font'] = 'Огромен';
+$lang['small font'] = 'Малък';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Несортиран списък : [ul][li]element[/li][/ul]';
+$lang['White'] = 'Бял';
+$lang['Yellow'] = 'Жълт';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Сортиран списък : [ol][li]element[/li][/ol]';
+$lang['Underline : [u]underline']['/u'] = 'Подчертан : [u]underline[/u]';
+$lang['large font'] = 'Голям';
+$lang['normal font'] = 'Нормален';
+$lang['Available options'] = 'Налични опции';
+$lang['Black'] = 'Черен';
+$lang['Blue'] = 'Син';
+$lang['Bold : [b]bold']['/b'] = 'Удебелен : [b]bold[/b]';
+$lang['Center : [center]center']['/center'] = 'Центрирано : [center]center[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'E-mail : [email]Email[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Цвят на шрифта';
+$lang['Font size : [size=X]text']['/size'] = 'Размер на шрифта';
+$lang['Green'] = 'Зелен';
+$lang['Grey'] = 'Сив';
+$lang['Italic : [i]italic']['/i'] = 'Наклонен';
+$lang['List element : [li]element']['/li'] = 'Елемент от списък : [li]element[/li]';
+$lang['Orange'] = 'Оранжев';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Параграф : [p]Paragraph[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Снимка : [img]picture[/img]';
+$lang['Purple'] = 'Пурпурен';
+$lang['Quote : [quote]quote']['/quote'] = 'Цитат : [quote]quote[/quote]';
+$lang['Red'] = 'Червен';
+$lang['Right : [right]right']['/right'] = 'Дясно : [right]right[/right]';
+?>
