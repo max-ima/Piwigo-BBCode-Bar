dd130498570c91dba24e93db976858cbc5e6b092
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-07 19:55:11 +0000

    [bbcode_bar] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@25824 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['List element : [li]element']['/li'] = 'Listero: [li]elemento[/li]';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Ordigita listo: [ol][li]elemento[/li][/ol]';
+$lang['Quote : [quote]quote']['/quote'] = 'Citi: [quote]citi[/quote]';
+$lang['Right : [right]right']['/right'] = 'Dekstro: [right]dekstro[/right]';
+$lang['Striped  : [s]striped']['/s'] = 'Striita: [s]striita[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL: [url=URL]Titolo[/url]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Neordigita listo: [ul][li]elemento[/li][/ul]';
+$lang['Underline : [u]underline']['/u'] = 'Substreki: [u]substreki[/u]';
+$lang['White'] = 'Blanka';
+$lang['Yellow'] = 'Flava';
+$lang['huge font'] = 'Larĝega';
+$lang['large font'] = 'Larĝa';
+$lang['normal font'] = 'Normala';
+$lang['small font'] = 'Eta';
+$lang['tiny font'] = 'Eteta';
+$lang['Available options'] = 'Disponeblaj opcioj';
+$lang['Black'] = 'Nigra';
+$lang['Blue'] = 'Blua';
+$lang['Bold : [b]bold']['/b'] = 'Grasa: [b]grasa[/b]';
+$lang['Center : [center]center']['/center'] = 'Centrigi: [center]centrigi[center]';
+$lang['E-mail : [email]Email']['/email'] = 'Retadreso: [email]Retadreso[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Tipara koloro: [color=color]teksto[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Tipara grandeco: [size=X]teksto[/size]';
+$lang['Green'] = 'Verda';
+$lang['Grey'] = 'Griza';
+$lang['Italic : [i]italic']['/i'] = 'Kursiva';
+$lang['Orange'] = 'Oranĝkolora';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Paragrafo: [p]Paragrafo[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Bildo: [img]bildo[/img]';
+$lang['Purple'] = 'Violkolora';
+$lang['Red'] = 'Ruĝa';
+?>
