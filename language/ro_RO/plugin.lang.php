d11c0caa64a14295df34db490c183e02d36fee0f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-27 18:58:29 +0000

    [bbcode_bar] Update ro_RO, thanks to : cheilenereinfo
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@27004 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Italic : [i]italic']['/i'] = '
+Italic : [i]italic[/i]
+';
+$lang['Center : [center]center']['/center'] = 'Centrat: [center]centrat[/center]';
+$lang['Bold : [b]bold']['/b'] = 'Ingrosat: [b]bold[/b]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL : [url=URL]Titlu[/url]';
+$lang['White'] = 'Alb';
+$lang['Yellow'] = 'Galben';
+$lang['huge font'] = 'Foarte mare';
+$lang['large font'] = 'Mare';
+$lang['normal font'] = 'Normal';
+$lang['tiny font'] = 'Minuscul';
+$lang['small font'] = 'Mic';
+$lang['Right : [right]right']['/right'] = 'Dreapta: [right]dreapta[/right]';
+$lang['Red'] = 'Rosu';
+$lang['Quote : [quote]quote']['/quote'] = 'Citat: [quote]citat[/quote]';
+$lang['Purple'] = 'Violet';
+$lang['Picture : [img]picture']['/img'] = 'Poza : [img]Poza[/img]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Paragraf: [p]Paragraf[/p]';
+$lang['Orange'] = 'Portocaliu';
+$lang['Grey'] = 'Gri';
+$lang['Green'] = 'Verde';
+$lang['Font size : [size=X]text']['/size'] = 'Dimensiune caracter: [size=X]text[/size]';
+$lang['Font color : [color=color]text']['/color'] = 'Culoare caracter: [color=color]text[/color]';
+$lang['E-mail : [email]Email']['/email'] = 'E-mail : [email]Email[/email]';
+$lang['Blue'] = 'Albastru';
+$lang['Black'] = 'Negru';
+$lang['Available options'] = 'Optuni disponibile';
+?>
