d326f2188286eaf66f3497d463a35955ae652284
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-09 17:52:06 +0000

    [bbcode_bar] Update fa_IR, thanks to : mnsb13600
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@27290 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Available options'] = 'گزینه های در دسترس';
+$lang['Striped  : [s]striped']['/s'] = 'راه راه';
+$lang['URL : [url=URL]Title']['/url'] = 'ادرس اینترنتی';
+$lang['Underline : [u]underline']['/u'] = 'زیر خط دار';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'لیست نامرتب';
+$lang['White'] = 'سفید ';
+$lang['Yellow'] = 'زرد';
+$lang['huge font'] = 'بسیار بزرگ ';
+$lang['large font'] = 'بزرگ ';
+$lang['normal font'] = 'نرمال ';
+$lang['small font'] = 'کوچک ';
+$lang['tiny font'] = 'کوچک تر';
+$lang['Black'] = 'سیاه ';
+$lang['Blue'] = 'ابی';
+$lang['Bold : [b]bold']['/b'] = 'تو پر';
+$lang['Center : [center]center']['/center'] = 'مرکز';
+$lang['E-mail : [email]Email']['/email'] = 'ایمیل';
+$lang['Font color : [color=color]text']['/color'] = 'رنگ فونت';
+$lang['Font size : [size=X]text']['/size'] = 'سایز فونت';
+$lang['Green'] = 'سبز';
+$lang['Grey'] = 'خاکستری';
+$lang['Italic : [i]italic']['/i'] = 'مورب';
+$lang['List element : [li]element']['/li'] = 'فهرست عنصر';
+$lang['Orange'] = 'نارنجی';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'لیست سفارشی';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'پاراگراف';
+$lang['Picture : [img]picture']['/img'] = 'عکس';
+$lang['Purple'] = 'ارغوانی';
+$lang['Quote : [quote]quote']['/quote'] = 'نقل قول';
+$lang['Red'] = 'قرمز';
+$lang['Right : [right]right']['/right'] = 'راست چین';
+?>
