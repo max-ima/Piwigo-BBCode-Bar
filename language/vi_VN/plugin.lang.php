37650a852004e0ac2c75ce9f1717c5799cc0d9fc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-28 04:48:40 +0000

    [bbcode_bar] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/bbcode_bar@28281 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Striped  : [s]striped']['/s'] = 'Gạch ngang  : [s]striped[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL : [url=URL]Title[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Gạch dưới : [u]underline[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Danh sách không có thứ tự : [ul][li]element[/li][/ul]';
+$lang['White'] = 'Trắng';
+$lang['Yellow'] = 'Vàng';
+$lang['huge font'] = 'Rất lớn';
+$lang['large font'] = 'Lớn';
+$lang['normal font'] = 'Bình thường';
+$lang['small font'] = 'Nhỏ';
+$lang['tiny font'] = 'Rất nhỏ';
+$lang['Available options'] = 'Các tùy chọn hiện có';
+$lang['Black'] = 'Đen';
+$lang['Blue'] = 'Xanh dương';
+$lang['Bold : [b]bold']['/b'] = 'Đậm : [b]bold[/b]';
+$lang['Center : [center]center']['/center'] = 'Canh giữa : [center]center[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'E-mail : [email]Email[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Màu chữ : [color=color]text[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Cỡ chữ : [size=X]text[/size]';
+$lang['Green'] = 'Xanh lá';
+$lang['Grey'] = 'Xám';
+$lang['Italic : [i]italic']['/i'] = 'In nghiêng : [i]italic[/i]';
+$lang['List element : [li]element']['/li'] = 'Thành phần của danh sách : [li]element[/li]';
+$lang['Orange'] = 'Cam';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Danh sách có thứ tự : [ol][li]element[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Đoạn văn : [p]Paragraph[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Hình ảnh : [img]picture[/img]';
+$lang['Purple'] = 'Tím';
+$lang['Quote : [quote]quote']['/quote'] = 'Trích dẫn : [quote]quote[/quote]';
+$lang['Red'] = 'Đỏ';
+$lang['Right : [right]right']['/right'] = 'Canh phải : [right]right[/right]';
