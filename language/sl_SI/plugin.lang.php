4e36b7ca342492e7b93e2a0ee7bf1a079f9cdb26
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-14 06:18:42 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Neurejen seznam : [ul][li]element[/li][/ul]';
+$lang['White'] = 'Bela';
+$lang['Yellow'] = 'Rumena';
+$lang['huge font'] = 'Ogromna';
+$lang['large font'] = 'Velika';
+$lang['normal font'] = 'Normalna';
+$lang['small font'] = 'Majhna';
+$lang['tiny font'] = 'Drobna';
+$lang['Orange'] = 'Oranžna';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Urejeni seznam : [ol][li]element[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Odstavek : [p]Odstavek[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Slika : [img]slika[/img]';
+$lang['Purple'] = 'Vijolična';
+$lang['Quote : [quote]quote']['/quote'] = 'Citat : [quote]citat[/quote]';
+$lang['Red'] = 'Rdeča';
+$lang['Right : [right]right']['/right'] = 'Desno : [right]desno[/right]';
+$lang['Striped  : [s]striped']['/s'] = 'Črtasto : [s]črtasto[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL : [url=URL]Naslov[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Podčrtano : [u]podčrtano[/u]';
+$lang['Black'] = 'Črna';
+$lang['Blue'] = 'Modra';
+$lang['Bold : [b]bold']['/b'] = 'Krepko : [b]krepko[/b]';
+$lang['Center : [center]center']['/center'] = 'Center : [center]center[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'E-pošta : [email]E-naslov[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Barva pisave: [color=color]besedilo[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Velikost pisave : [size=X]besedilo[/size]';
+$lang['Green'] = 'Zelena';
+$lang['Grey'] = 'Siva';
+$lang['Italic : [i]italic']['/i'] = 'Poševno : [i]poševno[/i]';
+$lang['List element : [li]element']['/li'] = 'Element seznama : [li]element[/li]';
+$lang['Available options'] = 'Razpoložljive možnosti';
